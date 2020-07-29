932891ba3f7ae7a3b144b5180225441af4c5292c
andreitraistaru <56828222+andreitraistaru@users.noreply.github.com>
2020-07-29 21:06:46 +0300

    Option menu created for implementing a settings activity.

--- app/src/main/java/com/timetable/MainActivity.java
+++ app/src/main/java/com/timetable/MainActivity.java
@@ -1,14 +1,32 @@
 package com.timetable;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Settings clicked!", Toast.LENGTH_SHORT).show();
+
+        return true;
     }
 }
